ff5770e17878378aaf7a545b07f579c586389e02
Pierre-Yves Le Traon <pyletraon@gmail.com>
2025-05-01 23:15:55 +0200

    fix: upped max queries in window in rateLimiter since it was blocking testing too quickly

--- middlewares/rateLimiter.js
+++ middlewares/rateLimiter.js
@@ -13,7 +13,7 @@ import rateLimit from "express-rate-limit";
 // Limiteur global pour toutes les routes
 export const globalLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 20, // Limite à 20 requêtes par fenêtre par IP (pou les besoins des tests, sinon limiter à 5, par exemple)
+  max: 50, // Limite à 50 requêtes par fenêtre par IP (pou les besoins des tests, sinon limiter à 5 ou 10, par exemple)
   message: {
     status: 429,
     message: "Trop de requêtes, veuillez réessayer dans 15 minutes",
@@ -23,7 +23,7 @@ export const globalLimiter = rateLimit({
 // Limiteur spécifique pour les routes d'authentification
 export const authLimiter = rateLimit({
   windowMs: 15 * 60 * 1000,
-  max: 20,
+  max: 50,
   message: {
     status: 429,
     message:
